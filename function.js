8e0247590755071ea90c1da8b016a86edb81793f
Rifat Bin Baccu <rifat18961190@gmail.com>
2024-02-19 02:55:42 +0600

    ri

---
+++ function.js
@@ -0,0 +1,147 @@
+document.getElementById("A1").onclick = function A1() {
+    seatSet('A1');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("A1").onclick = null;
+};
+
+document.getElementById("A2").onclick = function A2(){
+    seatSet('A2');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("A2").onclick = null;
+}
+document.getElementById("A3").onclick = function A3(){
+    seatSet('A3');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("A3").onclick = null;
+}
+document.getElementById("A4").onclick = function A4(){
+    seatSet('A4');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("A4").onclick = null;
+}
+document.getElementById("B1").onclick = function B1() {
+    seatSet('B1');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("B1").onclick = null;
+};
+
+document.getElementById("B2").onclick = function B2(){
+    seatSet('B2');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("B2").onclick = null;
+}
+document.getElementById("B3").onclick = function B3(){
+    seatSet('B3');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("B3").onclick = null;
+}
+document.getElementById("B4").onclick = function B4(){
+    seatSet('B4');
+    cuntSeit();
+    bookOfSeit();
+    document.getElementById("B4").onclick = null;
+}
+
+function C1(){
+    seatSet('C1');
+}
+function C2(){
+    seatSet('C2');
+}
+function C3(){
+    seatSet('C3');
+}
+function C4(){
+    seatSet('C4');
+}
+function D1(){
+    seatSet('D1');
+}
+function D2(){
+    seatSet('D2');
+}
+function D3(){
+    seatSet('D3');
+}
+function D4(){
+    seatSet('D4');
+}
+function E1(){
+    seatSet('E1');
+}
+function E2(){
+    seatSet('E2');
+}
+function E3(){
+    seatSet('E3');
+}
+function E4(){
+    seatSet('E4');
+}
+function F1(){
+    seatSet('F1');
+}
+function F2(){
+    seatSet('F2');
+}
+function F3(){
+    seatSet('F3');
+}
+function F4(){
+    seatSet('F4');
+}
+function G1(){
+    seatSet('G1');
+}
+function G2(){
+    seatSet('G2');
+}
+function G3(){
+    seatSet('G3');
+}
+function G4(){
+    seatSet('G4');
+}
+function H1(){
+    seatSet('H1');
+}
+function H2(){
+    seatSet('H2');
+}
+function H3(){
+    seatSet('H3');
+}
+function H4(){
+    seatSet('H4');
+}
+function I1(){
+    seatSet('I1');
+}
+function I2(){
+    seatSet('I2');
+}
+function I3(){
+    seatSet('I3');
+}
+function I4(){
+    seatSet('I4');
+}
+function J1(){
+    seatSet('J1');
+}
+function J2(){
+    seatSet('J2');
+}
+function J3(){
+    seatSet('J3');
+}
+function J4(){
+    seatSet('J4');
+}
---
+++ p-tickct.js
@@ -0,0 +1,123 @@
+function seatSet(ElementId){
+    const CallToSet = document.getElementById(ElementId);
+    CallToSet.classList.add('bg-orange-400');
+}
+// function removeBgClor(){
+//     const element = document.getElementById('Totalprice');
+//     element.classList.add('hidden');
+// }
+// function removeBgClo(){
+//     const element = document.getElementById('Totalprice');
+//     element.classList.remove('hidden',0);
+// }
+
+function cuntSeit(){
+    const cuntSet = document.getElementById('cunt');
+    const cuntAddValue = cuntSet.innerText;
+    const cuntIntValue = parseFloat(cuntAddValue);
+    // console.log(cuntIntValue);
+
+    const updatedLife = cuntIntValue + 1;
+    cuntSet.innerText = updatedLife;
+}
+function bookOfSeit(){
+    const cuntSe = document.getElementById('outseit');
+    const cuntAddValu = cuntSe.innerText;
+    const cuntIntValu = parseFloat(cuntAddValu);
+    // console.log(cuntIntValu);
+
+    const updatedLif = cuntIntValu - 1;
+    cuntSe.innerText = updatedLif;
+}
+
+
+const cards = document.querySelectorAll('.card');
+let TotlePrice = 0;
+for (let index = 0; index < cards.length; index++) {
+    const card = cards[index];
+    // console.log(element);
+    card.addEventListener('click', function () {
+        // console.log('ok')
+
+    const title = card.querySelector('h3').innerText;
+        const price = parseFloat(card.querySelector('h2').innerText);
+        // console.log(typeof(price));
+
+        
+        const titleContainer = document.getElementById('seitpush');
+        const p = document.createElement('p');
+        p.innerText =title;
+        titleContainer.appendChild(p);
+
+        const tit = card.querySelector('h1').innerText;
+        const titleContain = document.getElementById('EconomoyBus');
+        const h1 = document.createElement('h1');
+        h1.innerText = tit;
+        titleContain.appendChild(h1);
+
+        TotlePrice += price;
+        document.getElementById('Totalprice').innerText =TotlePrice.toFixed(2);
+        console.log(TotlePrice);
+    
+        const titl = card.querySelector('h2').innerText;
+
+      const titleContaine = document.getElementById('pricePush');
+        const h2 = document.createElement('h2');
+        h2.innerText = titl;
+        titleContaine.appendChild(h2);
+        document.querySelectorAll(".card").onclick = null;
+    });
+}
+
+const btn = document.getElementById('apply-btn');
+btn.addEventListener('click', function () {
+    const inputfield = document.getElementById('input-field').value;
+    if (inputfield === 'Couple 20') {
+        const pricePacent = TotlePrice * 0.20;
+        const allTotlePrice = document.getElementById('GrandTotal');
+        const priceAll = TotlePrice - pricePacent;
+        allTotlePrice.innerText = priceAll.toFixed(2);
+        // console.log(priceAll);
+        document.getElementById('input-field').value = "";
+        const showOption = document.getElementById('input&button');
+        showOption.classList.add('hidden');
+    }
+    else if (inputfield === 'NEW15') {
+        const pricePacentOf = TotlePrice * 0.15;
+        const allTotlePric = document.getElementById('GrandTotal');
+        const priceAl = TotlePrice - pricePacentOf;
+        allTotlePric.innerText = priceAl.toFixed(2);
+        document.getElementById('input-field').value = "";
+        const showOption = document.getElementById('input&button');
+        showOption.classList.add('hidden');
+    }
+    else {
+        document.getElementById('input-field').value = "";
+    }
+});
+
+function showPopapPoint(){
+    const showOption = document.getElementById('showPopap');
+    showOption.classList.remove('hidden');
+    document.getElementById('GrandTotal').value = "";
+}
+function showPopapPointhidden(){
+    const showOption = document.getElementById('showPopap');
+    showOption.classList.add('hidden');
+}
+// function IandBhidden(){
+//     const showOption = document.getElementById('input&button');
+//     showOption.classList.add('hidden');
+// }
+
+// document.getElementById('cleneallamunt').addEventListener('click', function clickliss(){
+//     const inputfiel = document.querySelector('span').value;
+//     console.log(inputfiel)
+//     if (inputfiel > 0){
+//         document.getElementById('input-field').value = ""; 
+//     }
+//     else{
+//         document.getElementById('input-field').value = ""; 
+//     }
+// })
+
