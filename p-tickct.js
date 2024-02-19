65d164aab42b03265e53665395bd5e92d12fade4
Rifat Bin Baccu <rifat18961190@gmail.com>
2024-02-19 22:31:03 +0600

    rifat

--- p-tickct.js
+++ p-tickct.js
@@ -84,6 +84,7 @@ btn.addEventListener('click', function () {
     }
     else {
         document.getElementById('input-field').value = "";
+        alert("Your 3 site buying and 1 add or not site buying !");
     }
 });
 
